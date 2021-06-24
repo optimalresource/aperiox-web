22329a6c98dc991676deda11afa1cc6b374fc695
optimalresource <joptimalresource@live.com>
2021-06-24 14:18:07 +0100

    Update Feature.js

--- components/Feature.js
+++ components/Feature.js
@@ -14,9 +14,9 @@ function Feature() {
         <div className="flex flex-col justify-center items-center w-screen h-auto bg-white overflow-hidden">
 
             <div className="flex flex-col lg:flex-row w-full justify-evenly items-center lg:py-20">
-                <FeatureItem imgUrl="/bsc.png" name="BBC News" url="https://bscscan.com/0x989d5fbbbf11f0bccd774cb364eb0349e7b1a56f"/>
-                <FeatureItem imgUrl="/pancakeswap.png" name="Tech Crunch"  url="https://exchange.pancakeswap.finance/#/swap?outputCurrency=0x989d5fbbbf11f0bccd774cb364eb0349e7b1a56f"/>
-                <FeatureItem imgUrl="coincodek.png" name="TechPoint Africa" url="https://coincodex.com/ico/apeirox/"/>
+                <FeatureItem imgUrl="/bsc.png" name="Bsc" url="https://bscscan.com/0x989d5fbbbf11f0bccd774cb364eb0349e7b1a56f"/>
+                <FeatureItem imgUrl="/pancakeswap.png" name="Pancakeswap"  url="https://exchange.pancakeswap.finance/#/swap?outputCurrency=0x989d5fbbbf11f0bccd774cb364eb0349e7b1a56f"/>
+                <FeatureItem imgUrl="coincodek.png" name="Coincodex" url="https://coincodex.com/ico/apeirox/"/>
             </div>
 
         </div>
